f11335bf9459b2b9a04cc1cef3eb0e843ce855de
sjostedtmax <max@sjostedt.nu>
2023-04-05 22:08:26 +0200

    first commit

---
+++ src/components/hand.svelte
@@ -0,0 +1,35 @@
+<script>
+	let src = '/media/hand.png';
+</script>
+
+<img class="floating" {src} alt="handen som pekar" />
+
+<style lang="scss">
+	img {
+		position: absolute;
+		width: 300px;
+		right: -20px;
+		bottom: 100px;
+	}
+
+	.floating {
+		animation-name: floating;
+		animation-duration: 3s;
+		animation-iteration-count: infinite;
+		animation-timing-function: ease-in-out;
+		margin-left: 30px;
+		margin-top: 5px;
+	}
+
+	@keyframes floating {
+		from {
+			transform: translate(0, 0px);
+		}
+		65% {
+			transform: translate(10px, 15px);
+		}
+		to {
+			transform: translate(0px, -0px);
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Hand from '../components/hand.svelte';
+</script>
+
+<svelte:head>
+	<title>svelte base</title>
+</svelte:head>
+
+<div class="main-view">
+	<Hand />
+</div>
+
+<style lang="scss">
+	.main-view {
+		z-index: 0;
+		width: 100vw;
+		height: 100vh;
+		background: #21201f;
+		display: flex;
+		user-select: none;
+	}
+</style>
